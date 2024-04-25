0dee6390ccf1fa1a72e04acb3ac0c59a06ff606b
Olivier Filangi <olivier.filangi@inrae.fr>
2024-04-25 15:02:32 +0200

    improve py

--- inference.py
+++ inference.py
@@ -1,4 +1,17 @@
+import gc
 import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM, AutoTokenizer
+from peft import PeftModel
+
+# model and adapters path
+model_hf = "microsoft/BioGPT-Large"
+lora_adapters = "mdelmas/BioGPT-Large-Natural-Products-RE-Diversity-synt-v1.0" # You can also try: mdelmas/BioGPT-Large-Natural-Products-RE-Extended-synt-v1.0
+
+# Load model and plug adapters using peft
+model = AutoModelForCausalLM.from_pretrained(model_hf, device_map={"":0})
+model = PeftModel.from_pretrained(model, lora_adapters)
+model = model.merge_and_unload()
+tokenizer = AutoTokenizer.from_pretrained(model_hf)
 
 def inference(text):
     device = torch.device("cuda")
@@ -21,14 +34,15 @@ def inference(text):
     input_tokens = tokenizer(input_text, return_tensors='pt')
     input_tokens.to(device)
 
-  # Generate
-  with torch.no_grad():
-    beam_output = model.generate(**input_tokens, **EVAL_GENERATION_ARGS)
-  output = tokenizer.decode(beam_output[0][len(input_tokens["input_ids"][0]):], skip_special_tokens=True)
+    # Generate
+    with torch.no_grad():
+        beam_output = model.generate(**input_tokens, **EVAL_GENERATION_ARGS)
+    
+    output = tokenizer.decode(beam_output[0][len(input_tokens["input_ids"][0]):], skip_special_tokens=True)
 
-  # Parse and print
-  rels = output.strip().split("; ")
-  for rel in rels:
-    print("- " + rel)
-  torch.cuda.empty_cache()
-  gc.collect()
+    # Parse and print
+    rels = output.strip().split("; ")
+    for rel in rels:
+        print("- " + rel)
+    torch.cuda.empty_cache()
+    gc.collect()
--- workflow.py
+++ workflow.py
@@ -1,36 +1,25 @@
-import gc
-import torch
-from transformers import AutoTokenizer, AutoModelForCausalLM, AutoTokenizer
-from peft import PeftModel
-
 from inference import * 
 
 import argparse
-
-# model and adapters path
-model_hf = "microsoft/BioGPT-Large"
-lora_adapters = "mdelmas/BioGPT-Large-Natural-Products-RE-Diversity-synt-v1.0" # You can also try: mdelmas/BioGPT-Large-Natural-Products-RE-Extended-synt-v1.0
-
-# Load model and plug adapters using peft
-model = AutoModelForCausalLM.from_pretrained(model_hf, device_map={"":0})
-model = PeftModel.from_pretrained(model, lora_adapters)
-model = model.merge_and_unload()
-tokenizer = AutoTokenizer.from_pretrained(model_hf)
+import os
+import json 
 
 if __name__ == "__main__":
     # Créer un analyseur d'arguments
     parser = argparse.ArgumentParser(description='')
 
     # Ajouter des arguments
-    parser.add_argument('--list_doi', type=str, help='list des doi')
-    #parser.add_argument('arg2', type=int, help='---')
-    #parser.add_argument('--option', type=str, help='Description de option')
+    parser.add_argument('--dump', type=str, help='JSON dum')
+    
+    args = parser.parse_args()
 
+    article_database = {}
+
+    # Vérifier si le fichier existe
+    if os.path.exists(args.dump):
+        # Charger les données JSON depuis le fichier
+        with open(args.dump, 'r') as fichier:
+            doi_database = json.load(fichier)
     
-    
-    filtre_bool = lambda x: x != "N/A"
-    list_clean_pmid = [x for x in list_pmid if filtre_bool(x)]
-    
-    for pmid in list_clean_pmid:
-        print(pmid)
-        get_title_abstract_text(pmid)
+    for article in article_database:
+        print(article)
