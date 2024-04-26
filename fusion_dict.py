27b8ef149cb84e3ee17ae96f2bcf9132e0dea5f8
Olivier Filangi <olivier.filangi@inrae.fr>
2024-04-26 09:37:01 +0200

    english translation

--- fusion_dict.py
+++ fusion_dict.py
@@ -1,19 +1,19 @@
-def fusionner_dictionnaires(d1, d2):
-    # Si l'un des dictionnaires est vide, retourner l'autre
+def merge_dictionaries(d1, d2):
+    # If either dictionary is empty, return the other
     if not d1:
         return d2
     if not d2:
         return d1
     
-    # Fusionner les clés de d2 dans d1
-    for cle, valeur in d2.items():
-        if cle in d1:
-            # Si la valeur est un dictionnaire, fusionner récursivement
-            if isinstance(valeur, dict) and isinstance(d1[cle], dict):
-                d1[cle] = fusionner_dictionnaires(d1[cle], valeur)
+    # Merge keys from d2 into d1
+    for key, value in d2.items():
+        if key in d1:
+            # If the value is a dictionary, merge recursively
+            if isinstance(value, dict) and isinstance(d1[key], dict):
+                d1[key] = merge_dictionaries(d1[key], value)
             else:
-                d1[cle] = valeur
+                d1[key] = value
         else:
-            d1[cle] = valeur
+            d1[key] = value
     
     return d1
